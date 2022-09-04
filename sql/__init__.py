c15a0b899e93244695f96c868ab97083d510b297
oreo <112804412+OREOnCHRE@users.noreply.github.com>
2022-09-04 22:44:25 +0530

    Update __init__.py

--- sql/__init__.py
+++ sql/__init__.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-DATABASE_URL = os.environ.get("postgres://tqaacpeqijpqza:e8912056731384cd45da2493658f4167c42c25401bf4ec12ee034ae7928c4357@ec2-107-23-76-12.compute-1.amazonaws.com:5432/d51l301i0s5efi")
+DATABASE_URL = os.environ.get(postgres://fwcetpcafvdpkp:05e903e12d8dcd2913d841775aeb09ee03915f98b09175ae92e313be20100d3f@ec2-44-193-178-122.compute-1.amazonaws.com:5432/d3j9q4ep29a405
+)
 
 def start() -> scoped_session:
     engine = create_engine(DATABASE_URL)
