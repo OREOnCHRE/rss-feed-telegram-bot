47f97504a099f9ee33e4c475d5a7217f28a20606
oreo <112804412+OREOnCHRE@users.noreply.github.com>
2022-09-05 15:53:48 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -2,9 +2,9 @@ import os
 import sys
 import feedparser
 from sql import db
-from time import sleep, time, MarkdownV2
+from time import sleep, time
 from dotenv import load_dotenv
-from pyrogram import Client, filters
+from pyrogram import Client, filters, MarkdownV2
 from pyrogram.errors import FloodWait
 from apscheduler.schedulers.background import BackgroundScheduler
 
