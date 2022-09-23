cb43729c37512f8e8333a3af5cb8bb9626d23357
oreo <112804412+OREOnCHRE@users.noreply.github.com>
2022-09-23 14:01:42 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -50,7 +50,7 @@ def create_feed_checker(feed_url):
                        # ↓ Edit this message as your needs.
                 # message = f"**{entry.title}**\n```{entry.link}```"
                 message = f"**{title}**\n`{tr_size}`\n\n⌈ [👀]({view_link})| [🔍]({tr_dl_link}) | [🔗]({magnet})⌋"
-                chat_id = message.chat.id
+                # chat_id = message.chat.id
             try:
                 app.send_message(log_channel, message, parse_mode= "markdown", disable_web_page_preview=True)
                 db.update_link(feed_url, entry.id)
