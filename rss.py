b83c9ef9e0fb4a62fc18a44951d8374e6eb22e3e
oreo <112804412+OREOnCHRE@users.noreply.github.com>
2022-10-01 22:51:39 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -50,8 +50,8 @@ def create_feed_checker(feed_url):
                 magnet = f"https://nyaasi.herokuapp.com/nyaamagnet/urn:btih:{info_hash}"
                        # ↓ Edit this message as your needs.
                 # message = f"**{entry.title}**\n```{entry.link}```"
-                global message         
                 message = f"**{title}**\n`{tr_size}`\n\n⌈ [👀]({view_link})| [🔍]({tr_dl_link}) | [🔗]({magnet})⌋"
+                global message  
                 # chat_id = message.chat.id
             try:
                 app.send_message(log_channel, message, parse_mode= "markdown", disable_web_page_preview=True)
